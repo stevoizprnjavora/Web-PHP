9f54c923fef851c40e9af0a13b2af31889fed53e
stevoizprnjavora <76901831+stevoizprnjavora@users.noreply.github.com>
2022-01-05 19:06:59 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,68 +1,26 @@
 <?php 
 	print '
-	<!DOCTYPE HTML>
-<html>
-<head>
-	<link rel="stylesheet" href="style.css">
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta name="description" content="Projekt">
-	<meta name="keywords" content="Home, html, News, Gallery">
-	<meta name="author" content="Zvonimir Ćosić">
-	<meta itemprop="name" content="Hello Example">
-	<meta itemprop="description" content="Some description">
-	<meta itemprop="image" content="Your URL">
-	<meta property="og:title" content="Hello Example">
-	<meta property="og:type" content="article">
-	<meta property="og:url" content="Your URL">
-	<meta property="og:image" content="Your URL">
-	<meta property="og:description" content="Some description">
-	<meta property="article:tag" content="keyword 1, keyword 2, keyword 3, keyword 4, ...">
-	<meta name="twitter:title" content="Hello Example">
-	<meta name="twitter:description" content="Some description">
-	<link rel="icon" href="favicon.ico" type="image/x-icon" />
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
-	<title>Zvonimir Ćosić</title>
-</head>
-<body>
-        <header>
-            <div class="logo">
-                <img src="gallery/logo.png">
-            </div>
-            <nav> ';
-                include("menu.php");
-            print '
-            </nav>
-        </header>
-        <main>
-            <h2>PHOTOGRAPHER BASED IN ZAGREB</h2>
-            <h3>Login</h3>
-	<div  id="contact">';
+	<h1>Sign In form</h1>
+	<div id="signin">';
 	
-	
-	$action = ( array_key_exists( 'action', $_POST) ? $_POST['action'] : "" );
-	if ($action == FALSE) {
+	if ($_POST['_action_'] == FALSE) {
 		print '
-		<form action="http://work2.eburza.hr/pwa/responzive-page/send-contact.php" id="contact_form" name="contact_form" method="POST">
+		<form action="" name="myForm" id="myForm" method="POST">
 			<input type="hidden" id="_action_" name="_action_" value="TRUE">
-	</div>
 
-			<label for="username">Korisničko ime : * <small></small></label>
-			<input type="text" id="username" name="username" pattern=".{5,10}" placeholder="Vaše korisničko ime ..." required>
-			<br>
+			<label for="username">Username:*</label>
+			<input type="text" id="username" name="username" value="" pattern=".{5,10}" required>
 									
-			<label for="username">Lozinka : * <small></small></label>
-			<input type="text" id="username" name="username" pattern=".{4,100}" placeholder="Vaša lozinka..." required>
-			<br>
+			<label for="password">Password:*</label>
+			<input type="password" id="password" name="password" value="" pattern=".{4,}" required>
 									
-			<br><br><input type="submit" value="Submit">
+			<input type="submit" value="Submit">
 		</form>';
 	}
 	else if ($_POST['_action_'] == TRUE) {
 		
 		$query  = "SELECT * FROM users";
-		$query .= " WHERE username='" .  $_POST['username'] . "'";
+		$query .= " WHERE username='" .  $_POST['username'] . "' AND archive='N'";
 		$result = @mysqli_query($MySQL, $query);
 		$row = @mysqli_fetch_array($result, MYSQLI_ASSOC);
 		
@@ -70,6 +28,8 @@
 			#password_verify https://secure.php.net/manual/en/function.password-verify.php
 			$_SESSION['user']['valid'] = 'true';
 			$_SESSION['user']['id'] = $row['id'];
+			# 1 - administrator; 2 - editor; 3 - user
+			$_SESSION['user']['role'] = $row['role'];
 			$_SESSION['user']['firstname'] = $row['firstname'];
 			$_SESSION['user']['lastname'] = $row['lastname'];
 			$_SESSION['message'] = '<p>Dobrodošli, ' . $_SESSION['user']['firstname'] . ' ' . $_SESSION['user']['lastname'] . '</p>';
@@ -85,18 +45,5 @@
 		}
 	}
 	print '
-	<footer>
-        <div class="footer">
-            <p>Social media:<br>
-                <a href="https://www.linkedin.com/company/ifixit" target="_blank"><img src="dr_mreze/linkedin.svg"
-                        alt="Linkedin" title="Linkedin" ></a>
-                <a href="https://twitter.com/ifixit" target="_blank"><img src="dr_mreze/twitter.svg" alt="Twitter"
-                        title="Twitter" ></a>
-                <a href="https://plus.google.com/113372414904624897851" target="_blank"><img src="dr_mreze/google.svg"
-                        alt="Google+" title="Google+"></a>
-            </p> 
-            <p>Copyright &copy; 2021. Zvonimir Cosic. <a href="https://google.hr"><svg width="10" height="22"
-                        viewBox="0 0 118 36" fill="none" xmlns="http://www.w3.org/2000/svg" focusable="false"></svg>
-        </div>
-    </footer>';
+	</div>';
 ?>
---
+++ users.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8111
+-- Generation Time: Jan 05, 2022 at 07:06 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `username`, `password`, `country`) VALUES
+(1, 'Damir', 'Eri', 'cico@gmail.com', 'Eroero', '123456', 'Norway');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
